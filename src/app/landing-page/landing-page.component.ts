47c25a797d0c4358853adc29a7773aad2e55b373
DorianMalguid <45062380+DorianMalguid@users.noreply.github.com>
2023-02-02 10:54:46 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { filter, interval, map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,11 +8,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  interval$!: Observable<string>;
+  elapsedSeconds!: number;
   constructor(private router: Router) {}
 
   onContinueToApp(): void {
     this.router.navigateByUrl('facesnaps');
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      tap((v) => (this.elapsedSeconds = v)),
+      filter((v) => v % 3 === 0),
+      map((v) => (v % 2 === 0 ? v + ', pair' : v + ', impair'))
+    );
+  }
 }
